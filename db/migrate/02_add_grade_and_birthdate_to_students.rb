061a2ca716e0eeb77d0933bdc3d1a679b868663b
Shamel Lakin <christlife4u@gmail.com>
2019-11-16 17:34:17 +0000

    Automatically backed up by Learn

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -3,6 +3,8 @@ class AddGradeAndBirthdateToStudents < ActiveRecord::Migration[5.1]
   def change
     create_table :students do |t|
       t.string :grade
+      t.string :birthdate
+    end
     end
   end 
 end 
